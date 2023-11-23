e81f07ca94c428d8a105e992fd6a3864f04cb3fd
user326 <a01708940@tec.mx>
2023-11-23 10:27:00 -0700

    Initial Commit

---
+++ EAPMobileiOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EAPMobileiOS
+//
+//  Created by user326 on 23/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EAPMobileiOS/EAPMobileiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EAPMobileiOSApp.swift
+//  EAPMobileiOS
+//
+//  Created by user326 on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct EAPMobileiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
